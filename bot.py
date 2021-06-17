56689deab010b953701a2a6bdd8a8384ae479b77
McSido <mcsido23@gmail.com>
2021-06-17 19:05:01 +0200

    Initial bot

--- bot.py
+++ bot.py
@@ -1,15 +1,41 @@
 import os
-
-import discord
+import random
 from dotenv import load_dotenv
+from discord.ext import commands
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-client.run(TOKEN)
+
+field = [["-", "-", "-"] for i in range(3)]
+
+
+@bot.command(name='ttt', help="TIC TAC TOE COMMAND")
+async def tic_tac_toe(ctx, x: int, y: int):
+    if(x not in range(1, 3) or y not in range(1, 3)):
+        await ctx.send("Only numbers between 1-3 work")
+        return
+
+    field[x-1][y-1] = "X"
+
+    await ctx.send(field)
+
+
+@tic_tac_toe.error
+async def tic_tac_toe_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Bad arguments')
+        await ctx.send(error)
+    else:
+        await ctx.send('Command didn\'t work')
+        await ctx.send(error)
+
+bot.run(TOKEN)
