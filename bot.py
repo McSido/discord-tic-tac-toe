cfeab9587e110d799f497b8e6e8b5d0dabd532f5
McSido <mcsido23@gmail.com>
2021-06-17 21:03:45 +0200

    Add initial winning conditions
    And winning messages

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import random
 from dotenv import load_dotenv
 from discord.ext import commands
-from game import Game
+from game import Game, verify
 
 
 load_dotenv()
@@ -15,29 +15,49 @@ bot = commands.Bot(command_prefix='!3')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-
+x_mapping = {"A": 0, "B": 1, "C": 2}
 game = Game()
 
+
 @bot.command(name='new', help="Start a new game")
-async def tic_tac_toe(ctx, x: int, y: int):
-    game = Game()
+async def new_game(ctx):
+    game.reset()
 
     await ctx.send("New game started")
+    await ctx.send(game.print())
 
 
-@bot.command(name='place', help="TIC TAC TOE COMMAND")
-async def tic_tac_toe(ctx, x: int, y: int):
-    if(x not in range(1, 3) or y not in range(1, 3)):
-        await ctx.send("Only numbers between 1-3 work")
+@bot.command(name='place', help="place your token")
+async def place(ctx: commands.Context, x_str: str, y: int):
+    name = ctx.author.nick if ctx.author.nick else ctx.author.name
+
+    valid, msg = verify(x_str, y)
+
+    if (not valid):
+        await ctx.send(msg)
         return
 
-    game.field[x-1][y-1] = "X"
+    x = x_mapping[x_str]
 
-    await ctx.send(game.field)
+    try:
+        game.setField(x, y-1)
+    except Exception as e:
+        await ctx.send(e)
+    else:
+        winner = game.check_winner()
+        if (winner):
+            await ctx.send(game.print())
+            await ctx.send(f"**{winner}** has won\n" +
+                           f"Congratulations {name}" +
+                           " :partying_face:")
+            game.reset()
+            await ctx.send(game.print())
+        else:
+            await ctx.send(game.print())
 
 
-@tic_tac_toe.error
-async def tic_tac_toe_error(ctx, error):
+@place.error
+async def place_error(ctx, error):
     if isinstance(error, commands.BadArgument):
         await ctx.send('Bad arguments')
         await ctx.send(error)
--- game.py
+++ game.py
@@ -1,5 +1,45 @@
+from typing import Optional
+
+
 class Game():
+    player_symbol = ["X", "O"]
 
     def __init__(self):
-        self.field = [["-", "-", "-"] for i in range(3)]
-        print("")
+        self.reset()
+
+    def reset(self):
+        self.field = [[" ", " ", " "] for i in range(3)]
+        self.current_player = 0
+
+    def setField(self, x: int, y: int):
+        if self.field[y][x] != " ":
+            raise Exception("Field already filled")
+
+        self.current_player = (self.current_player+1) % 2
+        self.field[y][x] = self.player_symbol[self.current_player]
+
+    def check_winner(self) -> Optional[str]:
+        # Diagonals
+        if (self.field[0][0] == self.field[1][1] == self.field[2][2] != " "):
+            return self.player_symbol[self.current_player]
+        if (self.field[0][2] == self.field[1][1] == self.field[2][0] != " "):
+            return self.player_symbol[self.current_player]
+
+        return None
+
+    def print(self) -> str:
+        return f"""```
+  A B C
+1 {"|".join(self.field[0])}
+  ------
+2 {"|".join(self.field[1])}
+  ------
+3 {"|".join(self.field[2])}```"""
+
+
+def verify(x_str: str, y: int):
+    if (x_str not in ["A", "B", "C"]):
+        return False, "Only A, B, C"
+    if(y not in [1, 2, 3]):
+        return False, "Only 1, 2, 3"
+    return True, None
