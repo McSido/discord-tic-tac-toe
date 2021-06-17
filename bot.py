5a81502e6a52546070864c97021ee9af66bd14df
McSido <mcsido23@gmail.com>
2021-06-17 19:11:58 +0200

    Initial split

--- bot.py
+++ bot.py
@@ -2,12 +2,13 @@ import os
 import random
 from dotenv import load_dotenv
 from discord.ext import commands
+from game import Game
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!3')
 
 
 @bot.event
@@ -15,18 +16,24 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
-field = [["-", "-", "-"] for i in range(3)]
+game = Game()
+
+@bot.command(name='new', help="Start a new game")
+async def tic_tac_toe(ctx, x: int, y: int):
+    game = Game()
+
+    await ctx.send("New game started")
 
 
-@bot.command(name='ttt', help="TIC TAC TOE COMMAND")
+@bot.command(name='place', help="TIC TAC TOE COMMAND")
 async def tic_tac_toe(ctx, x: int, y: int):
     if(x not in range(1, 3) or y not in range(1, 3)):
         await ctx.send("Only numbers between 1-3 work")
         return
 
-    field[x-1][y-1] = "X"
+    game.field[x-1][y-1] = "X"
 
-    await ctx.send(field)
+    await ctx.send(game.field)
 
 
 @tic_tac_toe.error
